da0f86afe3d8def881c64ad122b9523b7556b484
David Santos <davidhenriquehy@gmail.com>
2023-04-30 19:41:33 -0300

    Consulmindo api de filmes

--- filmes-em-cartaz/src/pages/Home/index.js
+++ filmes-em-cartaz/src/pages/Home/index.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react"
+import api from "../../services/api";
+///movie/now_playing?api_key=7ce101ef8a0104d3ad51a39c67ba3d19&language=pt-BR
+
+
 function Home() {
+    const [filmes, setFilmes ] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes(){
+            const response = await api.get('movie/now_playing', {
+                params:{
+                    api_key: '7ce101ef8a0104d3ad51a39c67ba3d19',
+                    language: 'pt-BR',
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results)
+        }
+
+        loadFilmes();
+    })
+
     return(
         <div>
             <h1>Bem vindo a Home</h1>
