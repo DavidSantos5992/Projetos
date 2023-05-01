c7ca8f70fb3b6697d2ed8c0285bfa814dcc05c4d
David Santos <davidhenriquehy@gmail.com>
2023-05-01 16:59:33 -0300

    incrementando css

--- filmes-em-cartaz/src/pages/Home/index.js
+++ filmes-em-cartaz/src/pages/Home/index.js
@@ -1,42 +1,46 @@
 import { useEffect, useState } from "react"
 import api from "../../services/api";
+import { Link } from 'react-router-dom'
+import "./home.css"
+
 ///movie/now_playing?api_key=7ce101ef8a0104d3ad51a39c67ba3d19&language=pt-BR
 
 
 function Home() {
-    const [filmes, setFilmes ] = useState([]);
+    const [filmes, setFilmes] = useState([]);
 
     useEffect(() => {
-        async function loadFilmes(){
+        async function loadFilmes() {
 
             const response = await api.get('movie/now_playing', {
-                params:{
+                params: {
                     api_key: '7ce101ef8a0104d3ad51a39c67ba3d19',
                     language: 'pt-BR',
                     page: 1,
                 }
             })
 
-            setFilmes(response.data.results.slice(0,10))
-          //  console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0, 10))
+            //  console.log(response.data.results.slice(0,10));
         }
 
         loadFilmes();
     })
 
-    return(
-      <div className="container">
-        <div className="lista-filmes">
-          {filmes.map((filme) => {
-            return(
-                <article key={filme.id}> 
-                    <strong>{filme.title}</strong>
-                    <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path }`} alt="filme.title" />
-                </article>
-            )
-          })}
+    return (
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt="filme.title" />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
-      </div>
     )
 }
 
