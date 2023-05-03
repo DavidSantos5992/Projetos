7b429aa3d83a370bbd06b21bc11a4d0d1f77a0f9
David Henrique Silva dos Santos <davidhenriquehy@gmail.com>
2023-05-03 20:03:34 -0300

    lista de favoritos

--- filmes-em-cartaz/src/pages/Favoritos/index.js
+++ filmes-em-cartaz/src/pages/Favoritos/index.js
@@ -1,8 +1,31 @@
-
+import { useState,useEffect } from "react";
+import './favoritos.css'
+import { Link } from "react-router-dom";
 function Favoritos() {
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem('@primeflix');
+        setFilmes(JSON.parse(minhaLista) || [])
+
+    }, [])
     return (
-        <div>
-            Meus Filmes Favoritos
+        <div className="meus-filmes">
+            <h1>Meus Filmes Favoritos</h1>
+
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <div className="">
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
