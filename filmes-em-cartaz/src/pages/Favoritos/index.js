62df68802d9cf464335aa88ebf39636629fb0ab6
David Henrique Silva dos Santos <davidhenriquehy@gmail.com>
2023-05-03 20:16:11 -0300

    Removendo filmes favoritos

--- filmes-em-cartaz/src/pages/Favoritos/index.js
+++ filmes-em-cartaz/src/pages/Favoritos/index.js
@@ -9,10 +9,18 @@ function Favoritos() {
         setFilmes(JSON.parse(minhaLista) || [])
 
     }, [])
+
+    function excluirFilme(id) {
+      let filtroFilmes = filmes.filter( (item) => {
+        return (item.id !== id)
+      })
+      setFilmes(filtroFilmes);
+      localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes))
+    }
     return (
         <div className="meus-filmes">
             <h1>Meus Filmes Favoritos</h1>
-
+            {filmes.length == 0 && <span>Você não possui nem um filme salvo :( </span>}
             <ul>
                 {filmes.map((item) => {
                     return(
@@ -20,7 +28,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div className="">
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() =>excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
