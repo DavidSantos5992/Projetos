99d463cde0e390cad8bc89d9104989ccf302118b
David Santos <davidhenriquehy@gmail.com>
2023-05-08 20:16:54 -0300

    route config

--- movie-show/src/App.js
+++ movie-show/src/App.js
@@ -1,6 +1,7 @@
 
 export default function App() {
   return (
+    
     <>
 
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -75,5 +76,6 @@ export default function App() {
         </div>
       </div>
     </>
+
   )
 }
