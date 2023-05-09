36a4e24ce07f0ff2eb8d8a3e0569ab370f9ee9c2
David Santos <davidhenriquehy@gmail.com>
2023-05-08 21:16:30 -0300

    config router Header

---
+++ movie-show/src/components/Header/index.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return(
+        <>
+        
+        <header className="flex items-center justify-between w-full h-11 bg-blue-300 box-border p-6">
+            <img src="" alt="" />
+
+            <div className="flex items-center justify-around w-96 -400 text-black capitalize">
+                <Link to='/'>Home</Link>
+                <Link to='/filmes'>Filmes em Cartaz</Link>
+                <Link to='/favoritos'>favoritos</Link>
+            </div>
+            
+            <button className="w-24 bg-blue-700 rounded-[10px] flex items-center justify-center">
+                <Link to='/login'>
+                <strong>Log in</strong>
+                </Link>
+            </button>
+
+        </header>
+        
+        </>
+    )
+    
+}
+
+export default Header
--- movie-show/src/routes/index.js
+++ movie-show/src/routes/index.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Header from "../components/Header";
 
 function RouterApp() {
     return(
         <>
             <BrowserRouter>
+            <Header/>
                 <Routes>
-
                     <Route path="/" element={ <Home/> } />
                     <Route path="/login" element={ <Login/> } />
-
                 </Routes>
             </BrowserRouter>
         </>
