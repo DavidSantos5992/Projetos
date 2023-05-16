36f926aed1ff7b48956b865ff06e708c5f32ef96
David Santos <davidhenriquehy@gmail.com>
2023-05-16 11:21:56 -0300

    import api

---
+++ movie-show/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//base da api  https://api.themoviedb.org/3/
+/* /movie/now_playing?api_key=7ce101ef8a0104d3ad51a39c67ba3d19&language=pt-BR */
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+})
+
+export default api
